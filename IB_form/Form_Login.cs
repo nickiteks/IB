3e47d4ad87be93610997ccf53840c6a37d4f43c4
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-10-01 13:51:45 +0400

    Update Form_Login.cs

--- IB_form/Form_Login.cs
+++ IB_form/Form_Login.cs
@@ -16,5 +16,19 @@ namespace IB_form
         {
             InitializeComponent();
         }
+
+        public void User_not_found()
+        {
+            MessageBox.Show("Неверное имя пользователя","Ошибка",MessageBoxButtons.OK,MessageBoxIcon.Error);
+        }
+        public void Password_wrong()
+        {
+            MessageBox.Show("Неверный пароль", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public void Password_wrong_validation()
+        {
+            MessageBox.Show("Неверный формат пароля", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
     }
 }
