96b58ea91d8b8e097f2a3b090165121dbc4ffb8f
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-10-22 16:05:50 +0400

    start

---
+++ BusinessLogic/Variant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Variant
+    {
+        string FIO = "Гладилин Никита Дмитриевич";
+        int variant = 2;
+        string reference_information = "";
+    }
+}
