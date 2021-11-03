1df76e385cd431baa8d4a5961471154d4db52c3f
KDANext <55459738+KDANext@users.noreply.github.com>
2021-11-03 15:52:35 +0400

    ффф

--- IB_form/Form_Information.Designer.cs
+++ IB_form/Form_Information.Designer.cs
@@ -40,6 +40,7 @@ namespace IB_form
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form_Information
             // 
--- IB_form/Form_Information.cs
+++ IB_form/Form_Information.cs
@@ -16,5 +16,10 @@ namespace IB_form
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
